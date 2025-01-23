59885d1c6036bc6f15c4da133eb591639225d39b
Breno Oliveira Dias <bre.oliveira.dias@gmail.com>
2025-01-23 14:53:04 -0300

    [ADD] Criando as rotas de Users e usando/criando Middlewares

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const express = require("express");
+const morgan = require("morgan");
 const fs = require("fs");
 
 const app = express();
 
 app.use(express.json());
+app.use(morgan("dev"));
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, "utf-8"));
 console.log(tours);
 
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
 const getAllTours = (req, res) => {
+  console.log(req.requestTime);
   res.status(200).json({
       status: "success",
       results: tours.length,
@@ -82,19 +90,55 @@ const deleteTour = (req, res) => {
   });
 };
 
+const getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "Rota não implementada ainda...",
+  });
+}
+
+const getUserById = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "Rota não implementada ainda...",
+  });
+}
+
+const createUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "Rota não implementada ainda...",
+  });
+}
+
+const updateUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "Rota não implementada ainda...",
+  });
+}
+
+const deleteUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "Rota não implementada ainda...",
+  });
+}
+
+
 // app.get("/api/v1/tours", getAllTours);
-
 // app.get("/api/v1/tours/:id", getTourByUser);
-
 // app.post("/api/v1/tours", createTour);
-
 // app.patch("/api/v1/tours/:id", updateTour);
-
-// app.delete("/api/v1/tours/:id", deleteTour)
+// app.delete("/api/v1/tours/:id", deleteTour);
 
 app.route("/api/v1/tours").get(getAllTours).post(createTour);
 
 app.route("/api/v1/tours/:id").get(getTourById).patch(updateTour).delete(deleteTour);
+
+app.route("/api/v1/users").get(getAllUsers).post(createUser);
+
+app.route("/api/v1/users/:id").get(getUserById).patch(updateUser).delete(deleteUser);
 
 const port = 3000;
 app.listen(port, () => {
