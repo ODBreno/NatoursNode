76f2b8486df47c541babd6f296df0410f8caeedb
Breno Oliveira Dias <bre.oliveira.dias@gmail.com>
2025-01-23 15:39:20 -0300

    [Refactor] Refatorando rotas e controllers/handlers

--- app.js
+++ app.js
@@ -1,146 +1,19 @@
 const express = require("express");
 const morgan = require("morgan");
-const fs = require("fs");
+
+const tourRouter = require("./routes/tourRoutes");
+const userRouter = require("./routes/userRoutes");
 
 const app = express();
-
 app.use(express.json());
 app.use(morgan("dev"));
-
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, "utf-8"));
-console.log(tours);
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
 
-const getAllTours = (req, res) => {
-  console.log(req.requestTime);
-  res.status(200).json({
-      status: "success",
-      results: tours.length,
-      data: {
-        tours,
-      },
-    });
-};
+app.use("/api/v1/tours", tourRouter);
+app.use("/api/v1/users", userRouter);
 
-const getTourById = (req, res) => {
-  console.log(req.params);
-  const id = req.params.id * 1;
-  const tour = tours.find(el => el.id === id);
-
-  if (!tour) {
-    return res.status(404).json({
-      status: "fail",
-      message: "ID não encontrado",
-    });
-  }
-
-  res.status(200).json({
-    status: "success",
-    data: {
-      tour,
-    },
-  });
-}
-
-const createTour = (req, res) => {
-  //console.log(req.body);
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId}, req.body);
-
-  tours.push(newTour);
-  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
-     res.status(201).json({
-       status: "success",
-       data: {
-         tour: newTour,
-       },
-     });
-  })
-}
-
-const updateTour = (req, res) => {
-  if (req.params.id*1 > tours.length) {
-    return res.status(404).json({
-      status: "fail",
-      message: "ID não encontrado",
-    });
-  }
-  res.status(200).json({
-    status: "success",
-    data: {
-      tour: "<Tour atualizado aqui...>",
-    },
-  });
-};
-
-const deleteTour = (req, res) => {
-  if (req.params.id*1 > tours.length) {
-    return res.status(404).json({
-      status: "fail",
-      message: "ID não encontrado",
-    });
-  }
-  res.status(204).json({
-    status: "success",
-    data: null
-  });
-};
-
-const getAllUsers = (req, res) => {
-  res.status(500).json({
-    status: "error",
-    message: "Rota não implementada ainda...",
-  });
-}
-
-const getUserById = (req, res) => {
-  res.status(500).json({
-    status: "error",
-    message: "Rota não implementada ainda...",
-  });
-}
-
-const createUser = (req, res) => {
-  res.status(500).json({
-    status: "error",
-    message: "Rota não implementada ainda...",
-  });
-}
-
-const updateUser = (req, res) => {
-  res.status(500).json({
-    status: "error",
-    message: "Rota não implementada ainda...",
-  });
-}
-
-const deleteUser = (req, res) => {
-  res.status(500).json({
-    status: "error",
-    message: "Rota não implementada ainda...",
-  });
-}
-
-
-// app.get("/api/v1/tours", getAllTours);
-// app.get("/api/v1/tours/:id", getTourByUser);
-// app.post("/api/v1/tours", createTour);
-// app.patch("/api/v1/tours/:id", updateTour);
-// app.delete("/api/v1/tours/:id", deleteTour);
-
-app.route("/api/v1/tours").get(getAllTours).post(createTour);
-
-app.route("/api/v1/tours/:id").get(getTourById).patch(updateTour).delete(deleteTour);
-
-app.route("/api/v1/users").get(getAllUsers).post(createUser);
-
-app.route("/api/v1/users/:id").get(getUserById).patch(updateUser).delete(deleteUser);
-
-const port = 3000;
-app.listen(port, () => {
-  console.log(`App rodando na porta ${port}...`);
-});
+module.exports = app;
---
+++ controllers/userController.js
@@ -0,0 +1,34 @@
+exports.getAllUsers = (req, res) => {
+    res.status(500).json({
+      status: "error",
+      message: "Rota não implementada ainda...",
+    });
+  }
+  
+exports.getUserById = (req, res) => {
+    res.status(500).json({
+        status: "error",
+        message: "Rota não implementada ainda...",
+    });
+}
+
+exports.createUser = (req, res) => {
+    res.status(500).json({
+        status: "error",
+        message: "Rota não implementada ainda...",
+    });
+}
+
+exports.updateUser = (req, res) => {
+    res.status(500).json({
+        status: "error",
+        message: "Rota não implementada ainda...",
+    });
+}
+
+exports.deleteUser = (req, res) => {
+    res.status(500).json({
+        status: "error",
+        message: "Rota não implementada ainda...",
+    });
+}
